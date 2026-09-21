31ad1035b65be40836115a4b55400bfb2c2ab6e3
PiStaker <azzajolly@gmail.com>
2026-09-21 18:55:58 +0100

    Update kds.js

--- functions/kds.js
+++ functions/kds.js
@@ -1,1 +1,12 @@
+xport async function onRequestGet({ env, params }) {
+  const key = params.user;
+  const data = await env.KDR.get(key);
+  return new Response(data || "{}", { status: 200 });
+}
 
+export async function onRequestPut({ env, params, request }) {
+  const key = params.user;
+  const body = await request.text();
+  await env.KDR.put(key, body);
+  return new Response("OK", { status: 200 });
+}
